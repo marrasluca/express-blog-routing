17ef6414e175307c10b73be914852a14e1ccc771
marrasluca <lmarras10@gmail.com>
2025-02-12 17:49:49 +0100

    end

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express'); //importazione di express
 const app = express();
 const port = 3000; //porta
+const postsRouter = require('./routers/posts');
 
 
 app.use(express.static('public')); //file statici
@@ -11,6 +12,8 @@ app.get('/', (req, res) => {
     res.send('Ciao a tutti!');
 
 });
+
+app.use('/api/posts', postsRouter);
 
 //avvio del server
 app.listen(port, () => {
--- routers/posts.js
+++ routers/posts.js
@@ -8,28 +8,28 @@ router.get('/', (req, res) => {
 
 //show
 router.get('/:id', (req, res) => {
-    res.send('Qui restituiremo un solo post')
+    res.send(`Qui restituiremo un solo post con id: ${req.params.id}`);
 });
 
 //store
 router.post('/', (req, res) => {
-    res.send('Qui restituiremo tutti i posts')
+    res.send('Qui creeremo un nuovo post')
 });
 
 //update
-router.update('/:id', (req, res) => {
-    res.send('Qui aggiorneremo un post')
+router.put('/:id', (req, res) => {
+    res.send(`Qui aggiorneremo un post con id: ${req.params.id}`)
 });
 
 //patch
 router.patch('/:id', (req, res) => {
-    res.send('Qui aggiorniamo in parte il post')
+    res.send(`Qui aggiorniamo in parte il post con id: ${req.params.id}`)
 });
 
 
 //delete
 router.delete('/:id', (req, res) => {
-    res.send('Qui eliminiamo un singolo post')
+    res.send(`Qui eliminiamo un singolo post con id: ${req.params.id}`)
 });
 
 module.exports = router;
